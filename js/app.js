33bf1f2c5827e47d01d33fcae4982a75e9545b28
chrisdzasc <christian.dzasc@gmail.com>
2025-11-07 17:03:45 -0600

    feat: Add playRound function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 
+// Keep track of the players score.
+let humanScore = 0;
+let computerScore = 0;
+
 /* Get the computer choice */
 function getComputerChoice() {
     const computerChoice = Math.random();
@@ -19,5 +23,30 @@ function getHumanChoice() {
     const humanChoice = prompt('Rock, Paper or Scissors?');
 
     // Return his choice
-    return humanChoice;
+    return humanChoice.toLowerCase();
+}
+
+// To play 1 round
+function playRound(humanChoice, computerChoice) {
+    
+    // If the userChoice is 'rock' and the cumputerChoice is 'scissors', user wins.
+    if(humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return humanScore += 1; // Add a point for the user.
+
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper') { // If the userChoice is 'scissors' and the cumputerChoice is 'paper', user wins.
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return humanScore += 1; // Add a point for the user.
+
+    } else if(humanChoice === 'paper' && computerChoice === 'rock') { // If the userChoice is 'paper' and the cumputerChoice is 'rock', user wins.
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return humanScore += 1; // Add a point for the user.
+
+    } else if(humanChoice === computerChoice) { // If both select the same option, it is a draw.
+        return `It's a draw!`;
+
+    } else { // Else, the user lose.
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        return computerScore += 1; // Add a point for the computer.
+    }
 }
