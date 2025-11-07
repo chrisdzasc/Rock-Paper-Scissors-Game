084763c52cf1ef83a9b6cab003a7c5fbd8bb0b7b
chrisdzasc <christian.dzasc@gmail.com>
2025-11-07 17:55:23 -0600

    feat: Add playGame function

--- js/app.js
+++ js/app.js
@@ -33,20 +33,37 @@ function playRound(humanChoice, computerChoice) {
     if(humanChoice === 'rock' && computerChoice === 'scissors') {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         return humanScore += 1; // Add a point for the user.
-
     } else if(humanChoice === 'scissors' && computerChoice === 'paper') { // If the userChoice is 'scissors' and the cumputerChoice is 'paper', user wins.
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         return humanScore += 1; // Add a point for the user.
-
     } else if(humanChoice === 'paper' && computerChoice === 'rock') { // If the userChoice is 'paper' and the cumputerChoice is 'rock', user wins.
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         return humanScore += 1; // Add a point for the user.
-
     } else if(humanChoice === computerChoice) { // If both select the same option, it is a draw.
-        return `It's a draw!`;
-
+        console.log(`It's a draw! No one gets point.`);
     } else { // Else, the user lose.
         console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
         return computerScore += 1; // Add a point for the computer.
     }
 }
+
+/* To play the game */
+function playGame() {
+
+    // Call playRound() 5 times to play.
+    for(let i = 0; i < 5; i++ ) {
+        playRound(getHumanChoice(), getComputerChoice()); 
+    }
+
+    // Announce who wins.
+    if(humanScore > computerScore) {
+        console.log(`Final score: ${humanScore} - ${computerScore}. You win!`);
+    } else if(humanScore === computerScore) {
+        console.log(`Final score: ${humanScore} - ${computerScore}. It's a draw!`)
+    } else {
+        console.log(`Final score: ${humanScore} - ${computerScore}. You lose!`);
+    }
+}
+
+// Invoke the function to play.
+playGame();
